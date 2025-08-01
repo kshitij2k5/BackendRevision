6b2934ae1b6ca6a15780d3c088f060186fac749d
Kshitij Sinha <kshitij2k5@gmail.com>
2025-08-02 02:26:10 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs=require('fs');
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<html>");
+    res.write("<head><title>Complete Coding!</title></head>");
+    res.write("<body><h1>Enter Your Details:</h1>");
+    res.write('<form action="/submit-details" method="POST">');
+    res.write('<input type="text" name="username" placeholder="Enter Your Name"><br><br>');
+
+    res.write('<input type="radio" name="gender" id="male" value="male">');
+    res.write('<label for="male">Male</label><br>');
+
+    res.write('<input type="radio" name="gender" id="female" value="female">');
+    res.write('<label for="female">Female</label><br><br>');
+
+    res.write('<input type="submit" value="Submit"> ');
+    res.write('<input type="reset" value="Reset">');
+
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }else if(req.url.toLowerCase()=="/submit-details" && req.method=="POST"){
+   fs.writeFile('user.txt',"Kshitij Sinha",()=>{
+    console.log("File Created");
+   })
+   res.statusCode = 302;
+   res.setHeader('Location','/');
+   return res.end();
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+});
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const http=require("http");
-const server=http.createServer((req,res)=>{
-  console.log(req);
-  res.end("Hello World");
-})
-const PORT=3000;
-server.listen(PORT,()=>{
-  console.log(`Server is running on PORT http://localhost:${PORT}`);
-})
+const http = require('http');
+const server = http.createServer((req, res) => {
+  //console.log(req.url,req.method,req.headers);
+  //res.setHeader('content-type','json');
+  res.setHeader('content-type', 'text/html');
+  if (req.url == "/") {
+   return  res.end('<h1>elcome to the Home Page</h1>');
+  }
+  else if (req.url == '/about') {
+    return res.end("<h2>Welcome to the about page</h2>")
+  }
+  else if (req.url == 'contact') {
+return res.end("<h2>welcome to the about page</h2>")
+  }
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    return res.end('<h1>Page not found</h1>');
+  }
+  
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1><h2>Like/share/subscribe</h2></body>');
+  res.write('</html>');
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
